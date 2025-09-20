43c616f6e98c803144c311b7a577414bd985b9e0
Fisun Aleksandr <texnikru@gmail.com>
2025-09-20 16:57:08 +1000

    * Идентификатор сессии может отсутствовать. Просто фиксирую возможную проблему.

--- src/Domain/SessionManager.php
+++ src/Domain/SessionManager.php
@@ -8,6 +8,7 @@ class SessionManager
 {
     public function getSession(): CustomerSession
     {
+        // @fixme Вообще-то возвращает пустую строку, если сессия не запущена. Требования не ясны откуда взять и как реагировать. Оставляю as-is.
         return new CustomerSession(session_id());
     }
 }
