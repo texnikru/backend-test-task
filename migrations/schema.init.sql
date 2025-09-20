42cd1343f189d9279dbfa21c06195554d86e9559
Fisun Aleksandr <texnikru@gmail.com>
2025-09-20 13:44:50 +1000

    * Пояснение к индексам и вообще схеме products - Убрал default, пускай клиенты приносят явно

--- migrations/schema.init.sql
+++ migrations/schema.init.sql
@@ -2,16 +2,19 @@ create table if not exists products
 (
     uuid varchar(255) not null comment 'UUID товара',
     category varchar(255) not null comment 'Категория товара',
-    is_active tinyint default 1  not null comment 'Флаг активности',
-    -- У товаров нет имён. Есть названия. Лучше обозвать колонку "title". Какие услуги? Ошибка в комменте?
-    name text default '' not null comment 'Тип услуги',
+    is_active tinyint not null comment 'Флаг активности',
+    -- У товаров нет имён. Есть названия. Лучше обозвать колонку "title"
+    name text not null comment 'Название товара',
     description text null comment 'Описание товара',
     thumbnail varchar(255) null comment 'Ссылка на картинку',
     price float not null comment 'Цена'
 )
     comment 'Товары';
 
+-- Скорее всего индекс бесполезный т.к. будет низкоселективным. Обычно все товары "активны".
 create index is_active_idx on products (is_active);
+
 create unique index uuid_idx on products (uuid) using hash;
--- Денормализованные данные. Лучше использовать числовой ключ, но можно обыграть через индекс типа хэш
+
+-- Денормализованные данные. Лучше использовать числовой ключ вида category_id, но можно обыграть через индекс типа хэш
 create index category_hash_idx on products (category) using hash;
