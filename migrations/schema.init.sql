3882c2f317bc533942136d9a4ab72080cc419c8b
Fisun Aleksandr <texnikru@gmail.com>
2025-09-20 16:50:04 +1000

    + Проставил кросы на файлы и номера строк в изначальной версии

--- migrations/schema.init.sql
+++ migrations/schema.init.sql
@@ -4,7 +4,6 @@ create table if not exists products
     category_id   int          not null comment 'ID категории товара',
     category_name varchar(255) not null comment 'Категория товара',
     is_active     tinyint      not null comment 'Флаг активности',
-    -- У товаров нет имён. Есть названия. Лучше обозвать колонку "title"
     name          text         not null comment 'Название товара',
     description   text null comment 'Описание товара',
     thumbnail     varchar(255) null comment 'Ссылка на картинку',
@@ -16,6 +15,6 @@ create index category_id_idx on products (category_id, is_active);
 
 create table if not exists categories
 (
-    id   int primary key,
+    id   int          not null primary key,
     name varchar(255) not null comment 'Название раздела',
 ) comment 'Категории товаров';
